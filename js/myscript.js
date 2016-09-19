abcc094b80ca7ea44b63fb17d5bdbd47b4b5ec56
Street Money <Street Money>
2016-09-19 12:22:46 +0300

    added prefixfree.min.js

--- js/myscript.js
+++ js/myscript.js
@@ -54,22 +54,21 @@ $(function () {
         }
     });
     
-    //use smooth scrolling when clicking on navigation
-    $('.navbar a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'')
-      && location.hostname === this.hostname) {
-        var $target = $(this.hash);
-        $target = $target.length && $target
-        || $('[name=' + this.hash.slice(1) +']');
-        if ($target.length) {
-          var targetOffset = $target.offset().top - topoffset;
-
-          $('html,body')
-          .animate({scrollTop: targetOffset}, 800);
-         return false;
-        }
-      }
-    });
+      //Use smooth scrolling when clicking on navigation
+    $('.navbar a[href*=\\#]:not([href=\\#])').click(function() {
+      if (location.pathname.replace(/^\//,'') === 
+        this.pathname.replace(/^\//,'') && 
+        location.hostname === this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top-topoffset+2
+          }, 500);
+          return false;
+        } //target.length
+      } //click function
+    }); //smooth scrolling
     
     //Automatically generate carousel indicators
     var x = 0;
